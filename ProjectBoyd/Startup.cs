899222046d59c30ddb504d1af3baddf695ec0fd1
Apolo2000 <dominic.apolo@emerson.com>
2022-09-29 15:12:02 -0500

    First commit with successful deploy - fix dbcontext bug - update sidebar user profile image - rename local db (ProjectBoyd_db)

--- ProjectBoyd/Startup.cs
+++ ProjectBoyd/Startup.cs
@@ -73,8 +73,8 @@ namespace ProjectBoyd {
             });*/
 
             services.AddAuthentication().AddMicrosoftAccount(microsoftOptions => {
-                microsoftOptions.ClientId = "ac07f7f5-53a6-4f01-869a-de2f3ca768d6";
-                microsoftOptions.ClientSecret = "6pS8Q~fTSVxKKB73KRszZrAZch.Z5D47Mq-JJaP5";
+                microsoftOptions.ClientId = "b6c5eac7-808a-4b24-a67e-c100cb9c1a4b";//"ac07f7f5-53a6-4f01-869a-de2f3ca768d6";//"c2ea8842-772a-4280-92a6-20db876b9e05";
+                microsoftOptions.ClientSecret = "Qnk8Q~NMqattEsAVQVJigoeA.TqCgeA5sVY4gato";//"6pS8Q~fTSVxKKB73KRszZrAZch.Z5D47Mq-JJaP5";//"umm7Q~iuAjQ54lfEzTwjskiKpRekPMyhEef99";
             });
 
             // Add any other services you'd like here
